048071570578432ff2becc102252256f0bb0a99f
Yousef Hesham <youssef100h@gmail.com>
2023-05-15 11:29:07 +0300

    create Many-to-many association for categ. and ar.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_07_173327) do
+ActiveRecord::Schema.define(version: 2023_05_15_081240) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
